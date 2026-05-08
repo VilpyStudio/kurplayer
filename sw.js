873decb8cbb7e0126d369b3f2c143d81043bfb03
bas de wildt <basdewildt@MacBook-Air-van-bas-4.local>
2026-05-08 11:15:40 +0200

    Vervang IndexedDB door Cache API voor audio opslag op iOS

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@ self.addEventListener('install', e => {
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE && k !== 'kurplayer-audio').map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
